eb92081fc271979682a40a65fffc4bf34c8db6f3
Marc Laheij <marclaheij@yahoo.com>
2016-11-21 12:55:52 +0100

    IOTHub Examples

---
+++ ConsoleApplication1/SendMessagesToCloud/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+
+namespace SendMessagesToCloud
+{
+
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "LAHEIJIOT.azure-devices.net";
+        static string deviceKey = "c6m3hQjniqIHdpDoCQfAHQN4cpzznCGQ9ZrM4G7QqAU=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("MWLSoftDevice1", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "MWLSoftDevice1",
+                    windSpeed = currentWindSpeed,
+                    something = 10,
+                    time = DateTime.Now
+
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(2000).Wait();
+            }
+        }
+
+    }
+}
